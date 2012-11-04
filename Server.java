9709721b34eb363f487b5ddee13a61c447a141be
Alex <aaralis@purdue.edu>
2012-11-04 01:41:12 -0400

    added comments to server.java

--- Server.java
+++ Server.java
@@ -3,55 +3,55 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 class Server{
-  public TCPChannel reqChannel;
-  public TCPChannel respChannel;
-  LinkedList<Integer> requesters;
-  LinkedList<Integer> responders;
-  LinkedList<String> requestersLocation;
-  LinkedList<String> respondersTeam;
+  public TCPChannel requesterChannel;//probably should have a get method for the request and responder channels but made them public insead because im lazy
+  public TCPChannel responderChannel;
+  LinkedList<Integer> requestersID;//list of backlogged requesters
+  LinkedList<Integer> respondersID;//list of backlogged responders
+  LinkedList<String> requestersLocation;//list of places that the requesters are located at.  Corresponds to the requester ID list
+  LinkedList<String> respondersTeam;//list of teams that the responders are on.  Corresponds to the responder ID list.
   
-  public Server(int reqPort, int recPort) {
-    reqChannel = new TCPChannel(reqPort);
-    respChannel = new TCPChannel(recPort);
-    requesters = new LinkedList<Integer>();
-    responders = new LinkedList<Integer>();
+  public Server(int reqPort, int recPort) {//init of all the object variables for the server
+    requesterChannel = new TCPChannel(reqPort);
+    responderChannel = new TCPChannel(recPort);
+    requestersID = new LinkedList<Integer>();
+    respondersID = new LinkedList<Integer>();
     requestersLocation = new LinkedList<String>();
     respondersTeam = new LinkedList<String>();
     
-    reqChannel.setMessageListener(new MessageListener(){
-      public void messageReceived(String message, int clientID) {
-        String currentRequestersLocation = message.substring(7, message.length());
+    requesterChannel.setMessageListener(new MessageListener(){
+      public void messageReceived(String message, int clientID) {//when a message is received by a requester then...
+        String currentRequestersLocation = message.substring(7, message.length()); //removes the starting Request: from the recieved message string
         
         try{
-        if(responders.size() == 0){
-          reqChannel.sendMessage("Searching:", clientID);
-          requesters.add(clientID);
-          requestersLocation.add(currentRequestersLocation);
-          
-        }else{
-          reqChannel.sendMessage("Assigned:" + respondersTeam.getFirst(), clientID);
-          respChannel.sendMessage("Assigned:" + currentRequestersLocation, responders.getFirst());
-        }
+          if(respondersID.size() == 0){//if there are responders on call then...
+            requesterChannel.sendMessage("Searching:", clientID); //tells the requester that it needs to wait
+            requestersID.add(clientID); //adds the requester ID to the backlog
+            requestersLocation.add(currentRequestersLocation);//adds the requester location to the backlog
+            
+          }else{//if there are no responders then...
+            requesterChannel.sendMessage("Assigned:" + respondersTeam.getFirst(), clientID);//sends a message to the requester telling them a responder has been assigned and what team that responder is on
+            responderChannel.sendMessage("Assigned:" + currentRequestersLocation, respondersID.getFirst());//tells the longest waiting responder they have been assigned and the location of the requester they are to help
+          }
         }catch(ChannelException e){
           e.printStackTrace();
         }
       }
     });
     
-    respChannel.setMessageListener(
-                                  new MessageListener(){
-      public void messageReceived(String message, int clientID) {
-        String currentRespondersTeam = message.substring(8, message.length());
+    responderChannel.setMessageListener(
+                                   new MessageListener(){
+      public void messageReceived(String message, int clientID) { //when a message is recieved form a responder then...
+        String currentRespondersTeam = message.substring(8, message.length());//removes the stating Response: form the recieved message string 
         
         try{
-        if(requesters.size() == 0){
-          respChannel.sendMessage("Searching:", clientID);
-          responders.add(clientID);
-          respondersTeam.add(currentRespondersTeam);
-        }else{
-          reqChannel.sendMessage("Assigned:" + currentRespondersTeam, requesters.getFirst());
-          respChannel.sendMessage("Assigned:" + requestersLocation.getFirst(), clientID);
-        }
+          if(requestersID.size() == 0){//if there are no backlogged requesters then...
+            responderChannel.sendMessage("Searching:", clientID);//tell the responder they need to wait
+            respondersID.add(clientID);//log the responder ID
+            respondersTeam.add(currentRespondersTeam);//log the responders team
+          }else{//if there are backlogged requesters then...
+            requesterChannel.sendMessage("Assigned:" + currentRespondersTeam, requestersID.getFirst());//tell the longest waiting requester a responder has been assigned and the team of that responder
+            responderChannel.sendMessage("Assigned:" + requestersLocation.getFirst(), clientID);//tell the responder they have been assigned and the location of the requester they are picking up
+          }
         }catch(ChannelException e){
           e.printStackTrace();
         }
@@ -65,8 +65,8 @@ class Server{
     while(true){
       if(s.nextLine().equals("exit")){
         try{
-        server.respChannel.close();
-        server.reqChannel.close();
+          server.responderChannel.close();
+          server.requesterChannel.close();
         }catch(ChannelException e){
           e.printStackTrace();
         }
